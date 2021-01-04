45056648b79c313a718a1a7f6028de9a88b94f7f
valmack <8445610+valmack@users.noreply.github.com>
2021-01-03 20:45:18 -0500

    feat: add option to install all contracts

--- src/installer.ts
+++ src/installer.ts
@@ -8,12 +8,15 @@ const OUTPUT_DIR = path.join(process.cwd(), './build/contracts');
  * Downloads compiled GM contracts from github and installs them into a project
  * @param contracts List of GM contract names
  */
-export async function installer(contracts: string[]) {
+export async function installer(contracts?: string[] | undefined) {
     console.log('Installing GM contracts...');
     try {
         await fs.promises.mkdir(OUTPUT_DIR, { recursive: true });
     } catch (err) {
         if (err.code != 'EEXIST') throw err;
+    }
+    if (contracts == undefined) {
+        contracts = await getAllContractNames();
     }
     await Promise.all(contracts.map(handleContract));
     function handleContract(contractName: string): Promise<void> {
@@ -24,6 +27,14 @@ export async function installer(contracts: string[]) {
         }
     }
     console.log('Done!');
+}
+
+async function getAllContractNames(): Promise<Array<string>> {
+    const contractListUrl = `https://raw.githubusercontent.com/xGodMode/contract-library/main/build/contracts.txt`;
+    const list = (await handleRequest(contractListUrl)).split('\n');
+    return list.map((name: string) => {
+        return name.split('.json')[0];
+    });
 }
 
 async function install(contractName: string) {
