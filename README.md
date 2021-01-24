c10fa2fb4a42a04da6dd6535650f67d128e997ab
valmack <8445610+valmack@users.noreply.github.com>
2021-01-23 19:17:27 -0500

    refactor: install by protocol instead of contract

--- README.md
+++ README.md
@@ -1,23 +1,22 @@
 Godmode Contract Library holds plug-and-play Godmode versions of smart contracts
 deployed on Ethereum mainnet.
 
-You can use this package to easily import individual (or all) GM contracts into
-your test suite.
+You can use this package to easily import GM contracts from individual (or all) protocols we support into your test suite.
 
 ## Usage
 
-In your project that uses Godmode for testing, define the GM contracts that you
+In your project that uses Godmode for testing, define the protocols that you
 want to pull in via your `package.json` file
 
 ```json
   "godmode": {
-    "contracts": [
-      "GMDai"
+    "protocols": [
+      "Maker"
     ]
   }
 ```
 
-Then run the following command to install those contracts into your project at `./build/contracts`
+Then run the following command to install the protocol's contracts into your project at `./build/protocols`
 > Make sure you run the npx command from the same directory where your `package.json` lives.
 
 ```sh
--- src/installer.ts
+++ src/installer.ts
@@ -2,40 +2,44 @@ import fs from 'fs';
 import path from 'path';
 import { handleRequest } from './utils';
 
-const OUTPUT_DIR = path.join(process.cwd(), './build/contracts');
-const CONTRACTS_LIST_PATH = path.join(__dirname, '../build/contracts.txt');
+const OUTPUT_DIR = path.join(process.cwd(), './build/protocols');
+const PROTOCOL_LIST_PATH = path.join(__dirname, '../build/protocols.txt');
 
 /**
- * Downloads compiled GM contracts from github and installs them into a project
- * @param contracts List of GM contract names
+ * Downloads compiled contracts in GM protocol files from github and installs
+ * them into a project
+ * @param protocols List of protocol names
  */
-export async function installer(contracts?: string[] | undefined) {
-    console.log('Installing GM contracts...');
+export async function installer(protocols?: string[] | undefined) {
+    console.log('Installing protocols for GM...');
     try {
         await fs.promises.mkdir(OUTPUT_DIR, { recursive: true });
     } catch (err) {
         if (err.code != 'EEXIST') throw err;
     }
-    const allContractNames = await getAllContractNames();
-    if (contracts == undefined) {
-        contracts = allContractNames;
+    const allProtocolNames = await getAllProtocolNames();
+    if (protocols == undefined) {
+        protocols = allProtocolNames;
     }
-    await Promise.all(contracts.map(handleContract));
-    function handleContract(contractName: string): Promise<void> {
+    await Promise.all(protocols.map(handleProtocol));
+    function handleProtocol(protocolName: string): Promise<void> {
         try {
-            if (!allContractNames.includes(contractName)) {
-                throw Error('No contract with that name exists');
+            if (!allProtocolNames.includes(protocolName)) {
+                throw Error('No protocol with that name exists');
             }
-            return install(contractName);
+            return install(protocolName);
         } catch (error) {
-            console.error(`Failed to install ${contractName}:`, error.message);
+            console.error(
+                `Failed to install protocol ${protocolName}:`,
+                error.message
+            );
         }
     }
     console.log('Done!');
 }
 
-async function getAllContractNames(): Promise<Array<string>> {
-    const list = await fs.promises.readFile(CONTRACTS_LIST_PATH);
+async function getAllProtocolNames(): Promise<Array<string>> {
+    const list = await fs.promises.readFile(PROTOCOL_LIST_PATH);
     return list
         .toString()
         .split('\n')
@@ -44,13 +48,13 @@ async function getAllContractNames(): Promise<Array<string>> {
         });
 }
 
-async function install(contractName: string) {
-    const contractFilePath = path.join(OUTPUT_DIR, contractName + '.json');
-    const contractUrl = `https://raw.githubusercontent.com/xGodMode/contract-library/main/build/contracts/${contractName}.json`;
-    const contract = await handleRequest(contractUrl, { json: true });
+async function install(protocolName: string) {
+    const protocolFilePath = path.join(OUTPUT_DIR, protocolName + '.json');
+    const protocolUrl = `https://raw.githubusercontent.com/xGodMode/contract-library/main/build/protocols/${protocolName}.json`;
+    const protocol = await handleRequest(protocolUrl, { json: true });
     await fs.promises.writeFile(
-        contractFilePath,
-        JSON.stringify(contract, null, 4)
+        protocolFilePath,
+        JSON.stringify(protocol, null, 4)
     );
-    console.log('Saved', contractName, 'to', contractFilePath);
+    console.log('Saved', protocolName, 'to', protocolFilePath);
 }
